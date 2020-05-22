6f369e2a395e6df15a40f755e0eb0ffbc813cd56
KareemSarhan <Kareemsarhan@gmail.com>
2020-05-22 10:59:38 +0200

    change url to have default value
    url = "http://api.musixmatch.com/ws/1.1/"

--- musixmatch/Musixmatch.cs
+++ musixmatch/Musixmatch.cs
@@ -6,13 +6,17 @@ namespace Musixmatch.NET
 {
     public class Musixmatch
     {
-        public String apiToken, url;
+        public String apiToken, url = "http://api.musixmatch.com/ws/1.1/";
+        public Musixmatch(String apiToken)
+        {
+            this.apiToken = apiToken;
+        }
         public Musixmatch(String apiToken, String url)
         {
             this.apiToken = apiToken;
             this.url = url;
         }
-            public String Get_url(String url)
+        public String Get_url(String url)
         {
             return this.url + url + String.Format("&apikey={0}",this.apiToken);
         }
