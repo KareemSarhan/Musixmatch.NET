ed03467845293800375be5a0f164aedf7b64637d
KareemSarhan <Kareemsarhan@gmail.com>
2020-05-22 11:21:15 +0200

    add Test.cs
    This will contain unittests for the wrapper

---
+++ Test/Test.cs
@@ -0,0 +1,20 @@
+using System.Threading.Tasks;
+using System;
+using System.Net.Http;
+using Musixmatch;
+namespace Musixmatch.NET
+{
+    public class Test
+    {            
+        static async Task Main(string[] args)
+        {
+            //U shoud set your Musixmatch Api Token as an environment variable using 
+            //Environment.SetEnvironmentVariable("MusixmatchApiToken", "YOUR API TOKEN");
+            Musixmatch X = new Musixmatch(Environment.GetEnvironmentVariable("MusixmatchApiToken"));
+            HttpClient client = new HttpClient();
+            // var responseString = await client.GetStringAsync("");
+            // Console.WriteLine(X.Get_url(X.album_tracks_get("20885699")));
+        }
+
+    }
+}
