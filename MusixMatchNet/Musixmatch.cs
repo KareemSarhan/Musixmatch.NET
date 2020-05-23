2d7b256de0d677f0d703ad524f6c775705868928
KareemSarhan <Kareemsarhan@gmail.com>
2020-05-23 16:44:55 +0200

    add constructor , Get_url , Class attributes
    Get_url completes the url

--- MusixMatchNet/Musixmatch.cs
+++ MusixMatchNet/Musixmatch.cs
@@ -10,6 +10,31 @@ namespace MusixmatchNet
 {
     public class Musixmatch
     {
-       
+        public String apiToken, url = "http://api.musixmatch.com/ws/1.1/";
+        String _format { get; set; } = "json";
+        String _callback { get; set; } = "callback";
+        public double page_size { get; set; } = 10;
+
+        public double page { get; set; } = 1;
+
+        HttpClient _httpclient = new HttpClient();
+        public Musixmatch(String apiToken)
+        {
+            this.apiToken = apiToken;
+        }
+        public Musixmatch(String apiToken, String url)
+        {
+            this.apiToken = apiToken;
+            this.url = url;
+        }
+        public String Get_url(String url)
+        {
+            return this.url + url + String.Format("&apikey={0}", this.apiToken);
+        }
+        public async Task<string> RequestAsync(String url)
+        {
+            return await _httpclient.GetStringAsync(url);
+        }
+
     }
 }
