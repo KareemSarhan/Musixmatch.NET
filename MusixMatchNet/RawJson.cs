74a927c3fd359f27719e93d00e92d449e9a87a2d
KareemSarhan <Kareemsarhan@gmail.com>
2020-05-23 16:34:40 +0200

    add RawJson , ReturnJson
    RawJson is equivalent to server response
    ReturnJson is what my Lib returns

---
+++ MusixMatchNet/RawJson.cs
@@ -0,0 +1,285 @@
+﻿namespace MusixmatchNet
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public class RawJson
+    {
+        [JsonProperty("message")]
+        public _Message _Message { get; set; }
+    }
+
+    public class _Message
+    {
+        [JsonProperty("header")]
+        public _Header _Header { get; set; }
+
+        [JsonProperty("body")]
+        public _Body _Body { get; set; }
+    }
+
+    public class _Body
+    {
+        [JsonProperty("album_list")]
+        public List<_AlbumList> _AlbumList { get; set; }
+
+        [JsonProperty("album")]
+        public _Album _Album { get; set; }
+
+        [JsonProperty("subtitle")]
+        public _Subtitle _Subtitle { get; set; }
+
+        [JsonProperty("artist_list")]
+        public List<_ArtistList> _ArtistList { get; set; }   
+        
+        [JsonProperty("artist")]
+        public _Artist _Artist { get; set; }
+    }
+    public class _ArtistList
+    {
+        [JsonProperty("artist")]
+        public _Artist _Artist { get; set; }
+    }
+
+    public class _Artist
+    {
+        [JsonProperty("artist_credits")]
+        public _ArtistCredits _ArtistCredits { get; set; }
+
+        [JsonProperty("artist_mbid")]
+        public string _ArtistMbid { get; set; }
+
+        [JsonProperty("artist_name")]
+        public string _ArtistName { get; set; }
+
+        [JsonProperty("secondary_genres")]
+        public _SecondaryGenres _SecondaryGenres { get; set; }
+
+        [JsonProperty("artist_alias_list")]
+        public List<_ArtistAliasList> _ArtistAliasList { get; set; }
+
+        [JsonProperty("artist_vanity_id")]
+        public string _ArtistVanityId { get; set; }
+
+        [JsonProperty("restricted")]
+        public long _Restricted { get; set; }
+
+        [JsonProperty("artist_country")]
+        public string _ArtistCountry { get; set; }
+
+        [JsonProperty("artist_comment")]
+        public string _ArtistComment { get; set; }
+
+        [JsonProperty("artist_name_translation_list")]
+        public List<_ArtistNameTranslationList> _ArtistNameTranslationList { get; set; }
+
+        [JsonProperty("artist_edit_url")]
+        public string _ArtistEditUrl { get; set; }
+
+        [JsonProperty("artist_share_url")]
+        public string _ArtistShareUrl { get; set; }
+
+        [JsonProperty("artist_id")]
+        public long _ArtistId { get; set; }
+
+        [JsonProperty("updated_time")]
+        public string _UpdatedTime { get; set; }
+
+        [JsonProperty("managed")]
+        public long _Managed { get; set; }
+
+        [JsonProperty("primary_genres")]
+        public _PrimaryGenres _PrimaryGenres { get; set; }
+
+        [JsonProperty("artist_twitter_url")]
+        public string _ArtistTwitterUrl { get; set; }
+
+        [JsonProperty("artist_rating")]
+        public long _ArtistRating { get; set; }
+    }
+
+    public class _ArtistAliasList
+    {
+        [JsonProperty("artist_alias")]
+        public string _ArtistAlias { get; set; }
+    }
+
+    public class _ArtistCredits
+    {
+        [JsonProperty("artist_list")]
+        public List<_ArtistList> _ArtistList { get; set; }
+    }
+
+    public class _ArtistNameTranslationList
+    {
+        [JsonProperty("artist_name_translation")]
+        public _ArtistNameTranslation _ArtistNameTranslation { get; set; }
+    }
+
+    public class _ArtistNameTranslation
+    {
+        [JsonProperty("language")]
+        public string _Language { get; set; }
+
+        [JsonProperty("translation")]
+        public string _Translation { get; set; }
+    }
+    public class _Subtitle
+    {
+        [JsonProperty("subtitle_body")]
+        public string _SubtitleBody { get; set; }
+
+        [JsonProperty("publisher_list")]
+        public List<string> _PublisherList { get; set; }
+
+        [JsonProperty("subtitle_language")]
+        public string _SubtitleLanguage { get; set; }
+
+        [JsonProperty("subtitle_language_description")]
+        public string _SubtitleLanguageDescription { get; set; }
+
+        [JsonProperty("subtitle_id")]
+        public long _SubtitleId { get; set; }
+
+        [JsonProperty("pixel_tracking_url")]
+        public string _PixelTrackingUrl { get; set; }
+
+        [JsonProperty("html_tracking_url")]
+        public string _HtmlTrackingUrl { get; set; }
+
+        [JsonProperty("restricted")]
+        public long _Restricted { get; set; }
+
+        [JsonProperty("lyrics_copyright")]
+        public string _LyricsCopyright { get; set; }
+
+        [JsonProperty("script_tracking_url")]
+        public string _ScriptTrackingUrl { get; set; }
+
+        [JsonProperty("subtitle_length")]
+        public long _SubtitleLength { get; set; }
+
+        [JsonProperty("updated_time")]
+        public string _UpdatedTime { get; set; }
+
+        [JsonProperty("writer_list")]
+        public List<string> _WriterList { get; set; }
+    }
+
+    public class _AlbumList
+    {
+        [JsonProperty("album", NullValueHandling = NullValueHandling.Ignore)]
+        public _Album _Album { get; set; }
+    }
+
+    public class _Album
+    {
+        [JsonProperty("album_id")]
+        public long _AlbumId { get; set; }
+
+        [JsonProperty("album_mbid")]
+        public string _AlbumMbid { get; set; }
+
+        [JsonProperty("album_name")]
+        public string _AlbumName { get; set; }
+
+        [JsonProperty("album_rating")]
+        public long _AlbumRating { get; set; }
+
+        [JsonProperty("album_release_date")]
+        public string _AlbumReleaseDate { get; set; }
+
+        [JsonProperty("artist_id")]
+        public long _ArtistId { get; set; }
+
+        [JsonProperty("artist_name")]
+        public string _ArtistName { get; set; }
+
+        [JsonProperty("primary_genres")]
+        public _PrimaryGenres _PrimaryGenres { get; set; }
+
+        [JsonProperty("secondary_genres")]
+        public _PrimaryGenres _SecondaryGenres { get; set; }
+
+        [JsonProperty("album_pline")]
+        public string _AlbumPline { get; set; }
+
+        [JsonProperty("album_copyright")]
+        public string _AlbumCopyright { get; set; }
+
+        [JsonProperty("album_label")]
+        public string _AlbumLabel { get; set; }
+
+        [JsonProperty("restricted")]
+        public long _Restricted { get; set; }
+
+        [JsonProperty("updated_time")]
+        public DateTimeOffset _UpdatedTime { get; set; }
+
+        [JsonProperty("album_coverart_100x100")]
+        public string _AlbumCoverart100X100 { get; set; }
+    }
+
+    public class _PrimaryGenres
+    {
+        [JsonProperty("music_genre_list")]
+        public List<_MusicGenreList> _MusicGenreList { get; set; }
+    }
+    public class _SecondaryGenres
+    {
+        [JsonProperty("music_genre_list")]
+        public List<_MusicGenreList> _MusicGenreList { get; set; }
+    }
+    public class _MusicGenreList
+    {
+        [JsonProperty("music_genre")]
+        public _MusicGenre _MusicGenre { get; set; }
+    }
+
+    public class _MusicGenre
+    {
+        [JsonProperty("music_genre_id")]
+        public long _MusicGenreId { get; set; }
+
+        [JsonProperty("music_genre_parent_id")]
+        public long _MusicGenreParentId { get; set; }
+
+        [JsonProperty("music_genre_name")]
+        public string _MusicGenreName { get; set; }
+
+        [JsonProperty("music_genre_name_extended")]
+        public string _MusicGenreNameExtended { get; set; }
+
+        [JsonProperty("music_genre_vanity")]
+        public string _MusicGenreVanity { get; set; }
+    }
+
+    public class _Header
+    {
+        [JsonProperty("status_code")]
+        public long _StatusCode { get; set; }
+
+        [JsonProperty("execute_time")]
+        public long _ExecuteTime { get; set; }
+
+        [JsonProperty("available")]
+        public long _Available { get; set; }
+    }
+
+
+    internal static class _Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+}
---
+++ MusixMatchNet/ReturnJson.cs
@@ -0,0 +1,241 @@
+﻿namespace MusixmatchNet
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public class ReturnJson
+    {
+        public static Album From_AlbumToAlbum(_Album _Album) 
+        {
+            Album TempAlbum = new Album(_Album);
+            if (_Album._PrimaryGenres != null)
+                    foreach (_MusicGenreList _MusicGenreList in _Album._PrimaryGenres._MusicGenreList)
+                {
+                    TempAlbum.MusicGenreList.Add(new MusicGenre(_MusicGenreList));
+                }
+            if(_Album._SecondaryGenres != null)
+                foreach (_MusicGenreList _MusicGenreList in _Album._SecondaryGenres._MusicGenreList)
+                {
+                    TempAlbum.MusicGenreList.Add(new MusicGenre(_MusicGenreList));
+                }
+            return TempAlbum;
+        }
+
+        public static List<Album> FromRawJsonToAlbumList(RawJson RawJson) 
+        {
+            List<Album> AlbumList = new List<Album>();
+            if(RawJson._Message._Body._AlbumList!=null)
+                foreach (_AlbumList _AlbumList in RawJson._Message._Body._AlbumList)
+                {
+                    AlbumList.Add(From_AlbumToAlbum(_AlbumList._Album));
+                }
+            return AlbumList;
+        }
+        public static Artist From_ArtistToArtist(_Artist _Artist)
+        {
+            Artist TempArtist = new Artist(_Artist);
+            if (_Artist._PrimaryGenres != null)
+                foreach (_MusicGenreList _MusicGenreList in _Artist._PrimaryGenres._MusicGenreList)
+                {
+                    TempArtist.MusicGenreList.Add(new MusicGenre(_MusicGenreList));
+                }
+            if (_Artist._SecondaryGenres != null)
+                foreach (_MusicGenreList _MusicGenreList in _Artist._SecondaryGenres._MusicGenreList)
+                {
+                    TempArtist.MusicGenreList.Add(new MusicGenre(_MusicGenreList));
+                }
+            if (_Artist._ArtistAliasList != null)
+                foreach (_ArtistAliasList _ArtistAliasList in _Artist._ArtistAliasList)
+                {
+                    TempArtist.ArtistAliasList.Add(_ArtistAliasList._ArtistAlias);
+                }
+            if (_Artist._ArtistNameTranslationList != null)
+                foreach (_ArtistNameTranslationList _ArtistNameTranslationList in _Artist._ArtistNameTranslationList)
+                {
+                        TempArtist.ArtistNameTranslationList.Add(new ArtistNameTranslation(_ArtistNameTranslationList));
+                }
+            return TempArtist;
+        }
+        public static List<Artist> FromRawJsonToArtistList(RawJson RawJson) 
+        {
+            List<Artist> ArtistList = new List<Artist>();
+            if (RawJson._Message._Body._ArtistList != null)
+                foreach (_ArtistList _ArtistList in RawJson._Message._Body._ArtistList)
+                {
+                    ArtistList.Add(From_ArtistToArtist(_ArtistList._Artist));
+                }
+            return ArtistList;
+        }
+    }
+
+    public class Artist 
+    {
+        public Artist(_Artist _Artist)
+        {
+            this.ArtistVanityId = _Artist._ArtistVanityId;
+            this.ArtistCountry = _Artist._ArtistCountry;
+            this.ArtistComment = _Artist._ArtistComment;
+            this.ArtistEditUrl = _Artist._ArtistEditUrl;
+            this.ArtistMbid = _Artist._ArtistMbid;
+            this.ArtistName = _Artist._ArtistName;
+            this.Restricted = _Artist._Restricted;
+            this.UpdatedTime = _Artist._UpdatedTime;
+            this.ArtistEditUrl = _Artist._ArtistEditUrl;
+            this.ArtistShareUrl = _Artist._ArtistShareUrl;
+            this.ArtistId = _Artist._ArtistId;
+            this.UpdatedTime = _Artist._UpdatedTime;
+            this.Managed = _Artist._Managed;
+            this.ArtistTwitterUrl = _Artist._ArtistTwitterUrl;
+            this.ArtistRating = _Artist._ArtistRating;
+            this.MusicGenreList =new List<MusicGenre>();
+            this.ArtistAliasList=new List<String>();
+            this.ArtistNameTranslationList=new List<ArtistNameTranslation>();
+        }
+
+        public ArtistCredits ArtistCredits { get; set; }
+
+        public string ArtistMbid { get; set; }
+
+        public string ArtistName { get; set; }
+
+        public List<MusicGenre> MusicGenreList { get; set; }
+
+        public List<String> ArtistAliasList { get; set; }
+
+        public string ArtistVanityId { get; set; }
+
+        public long Restricted { get; set; }
+
+        public string ArtistCountry { get; set; }
+
+        public string ArtistComment { get; set; }
+
+        public List<ArtistNameTranslation> ArtistNameTranslationList { get; set; }
+
+        public string ArtistEditUrl { get; set; }
+
+        public string ArtistShareUrl { get; set; }
+
+        public long ArtistId { get; set; }
+
+        public string UpdatedTime { get; set; }
+
+        public long Managed { get; set; }
+
+        public string ArtistTwitterUrl { get; set; }
+
+        public long ArtistRating { get; set; }
+
+    }
+    public class ArtistNameTranslation
+    {
+        public ArtistNameTranslation(_ArtistNameTranslationList _ArtistNameTranslationList)
+        {
+            this.Language = _ArtistNameTranslationList._ArtistNameTranslation._Language;
+            this.Translation = _ArtistNameTranslationList._ArtistNameTranslation._Language;
+        }
+        public string Language { get; set; }
+
+        public string Translation { get; set; }
+    }
+    public class ArtistCredits
+    {
+        [JsonProperty("artist_list")]
+        public List<Artist> ArtistList { get; set; }
+    }
+    public class Album
+    {
+        public long AlbumId { get; set; }
+
+        public string AlbumMbid { get; set; }
+
+        public string AlbumName { get; set; }
+
+        public long AlbumRating { get; set; }
+
+        public string AlbumReleaseDate { get; set; }
+
+        public long ArtistId { get; set; }
+
+        public string ArtistName { get; set; }
+
+        public List<MusicGenre> MusicGenreList { get; set; }
+
+        public string AlbumPline { get; set; }
+
+        public string AlbumCopyright { get; set; }
+
+        public string AlbumLabel { get; set; }
+
+        public long Restricted { get; set; }
+
+        public DateTimeOffset UpdatedTime { get; set; }
+
+        public string AlbumCoverart { get; set; }
+
+        public Album() { }
+
+        public Album(_AlbumList _AlbumList) 
+        {
+            this.AlbumCopyright=_AlbumList._Album._AlbumCopyright;
+            this.AlbumId=_AlbumList._Album._AlbumId;
+            this.AlbumLabel=_AlbumList._Album._AlbumLabel;
+            this.AlbumMbid=_AlbumList._Album._AlbumMbid;
+            this.AlbumName=_AlbumList._Album._AlbumName;
+            this.AlbumPline=_AlbumList._Album._AlbumPline;
+            this.AlbumRating=_AlbumList._Album._AlbumRating;
+            this.AlbumReleaseDate=_AlbumList._Album._AlbumReleaseDate;
+            this.ArtistId=_AlbumList._Album._ArtistId;
+            this.ArtistName = _AlbumList._Album._ArtistName;
+            this.Restricted = _AlbumList._Album._Restricted;
+            this.UpdatedTime = _AlbumList._Album._UpdatedTime;
+            this.MusicGenreList = new List<MusicGenre>();
+        }
+        public Album(_Album _Album)
+        {
+            this.AlbumCopyright = _Album._AlbumCopyright;
+            this.AlbumId = _Album._AlbumId;
+            this.AlbumLabel = _Album._AlbumLabel;
+            this.AlbumMbid = _Album._AlbumMbid;
+            this.AlbumName = _Album._AlbumName;
+            this.AlbumPline = _Album._AlbumPline;
+            this.AlbumRating = _Album._AlbumRating;
+            this.AlbumReleaseDate = _Album._AlbumReleaseDate;
+            this.ArtistId = _Album._ArtistId;
+            this.ArtistName = _Album._ArtistName;
+            this.Restricted = _Album._Restricted;
+            this.UpdatedTime = _Album._UpdatedTime;
+            this.MusicGenreList = new List<MusicGenre>();
+        }
+
+    }
+
+    public class MusicGenre
+    {
+        public long MusicGenreId { get; set; }
+
+        public long MusicGenreParentId { get; set; }
+
+        public string MusicGenreName { get; set; }
+
+        public string MusicGenreNameExtended { get; set; }
+
+        public string MusicGenreVanity { get; set; }
+        public MusicGenre() { }
+
+        public MusicGenre(_MusicGenreList _MusicGenreList) 
+        {
+            this.MusicGenreId = _MusicGenreList._MusicGenre._MusicGenreId;
+            this.MusicGenreName = _MusicGenreList._MusicGenre._MusicGenreName;
+            this.MusicGenreNameExtended = _MusicGenreList._MusicGenre._MusicGenreNameExtended;
+            this.MusicGenreParentId = _MusicGenreList._MusicGenre._MusicGenreParentId;
+            this.MusicGenreVanity = _MusicGenreList._MusicGenre._MusicGenreVanity;
+        }
+    }
+
+
+
+}
